3ac7af35bb8d32136cb09a317b1193d7af17ab01
SMUTIS <sandramutissanabria@gmail.com>
2021-08-30 17:18:08 +0000

    inicial

--- src/app.js
+++ src/app.js
@@ -2,10 +2,30 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+document.querySelector(".btn").addEventListener("click", () => {
+  document.querySelector("#name").innerHTML = domainName();
+});
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+let pronoun = ["the", "this", "that", "last"];
+let adj = ["great", "pink", "big"];
+let noun = ["kitty", "swag", "punk"];
+let ending = [".com", ".net", ".us"];
+function domainName() {
+  let i = 0;
+  let j = 0;
+  let k = 0;
+  let l = 0;
+
+  let generate = "";
+
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+        for (let l = 0; l < ending.length; l++) {
+          generate += pronoun[i] + adj[j] + noun[k] + ending[l] + " <br>";
+        }
+      }
+    }
+  }
+  return generate;
+}
